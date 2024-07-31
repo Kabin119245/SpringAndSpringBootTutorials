5b38e269930a3ecef6f754391595cd7e0242cac9
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-07-31 18:03:54 +0545

    initialized

---
+++ Spring-Security-Demo/Spring-Security-Demo/src/main/java/com/kabin/Spring_Security_Demo/SpringSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kabin.Spring_Security_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDemoApplication.class, args);
+	}
+
+}
