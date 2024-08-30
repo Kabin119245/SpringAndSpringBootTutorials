958188fb7f29b1dd0927143b727d2f180cc94b7d
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-08-30 22:11:14 +0545

    verificationn, BCrypt PW Encoder

--- SpringSecTelusko/src/main/java/com/kabin/SpringSecTelusko/controller/UserController.java
+++ SpringSecTelusko/src/main/java/com/kabin/SpringSecTelusko/controller/UserController.java
@@ -3,6 +3,7 @@ package com.kabin.SpringSecTelusko.controller;
 import com.kabin.SpringSecTelusko.model.Users;
 import com.kabin.SpringSecTelusko.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +17,11 @@ public class UserController {
     @PostMapping("/register")
     public Users register(@RequestBody Users users) {
         return service.register(users);
+    }
+
+    //return all users
+    @GetMapping("/allusers")
+    public Iterable<Users> getAllUsers() {
+        return service.getAllUsers();
     }
 }
--- SpringSecTelusko/src/main/java/com/kabin/SpringSecTelusko/service/UserService.java
+++ SpringSecTelusko/src/main/java/com/kabin/SpringSecTelusko/service/UserService.java
@@ -3,6 +3,7 @@ package com.kabin.SpringSecTelusko.service;
 import com.kabin.SpringSecTelusko.model.Users;
 import com.kabin.SpringSecTelusko.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,15 @@ public class UserService {
     @Autowired
     private UserRepo userRepo;
 
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
     public Users register(Users user) {
+        user.setPassword(encoder.encode(user.getPassword()));
       return userRepo.save(user);
+    }
+
+    //return list of users
+    public Iterable<Users> getAllUsers() {
+        return userRepo.findAll();
     }
 }
