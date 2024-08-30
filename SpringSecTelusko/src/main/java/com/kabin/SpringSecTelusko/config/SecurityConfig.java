7678e11e66df5b8b5f737785a68ac40448b73f72
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-08-30 18:57:44 +0545

    Bean-UserDetailsService added

--- SpringSecTelusko/src/main/java/com/kabin/SpringSecTelusko/config/SecurityConfig.java
+++ SpringSecTelusko/src/main/java/com/kabin/SpringSecTelusko/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,16 +17,30 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-       return http
-               .csrf(customizer -> customizer.disable())
-               .authorizeHttpRequests(request -> request.anyRequest().authenticated())
-               // .formLogin(Customizer.withDefaults())
-               .httpBasic(Customizer.withDefaults())
-               .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+      return http
+            .csrf(customizer -> customizer.disable())
+            .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+            // .formLogin(Customizer.withDefaults())
+            .httpBasic(Customizer.withDefaults())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .build();
+   }
+
+   @Bean
+   public UserDetailsService userDetailsService() {
 
 
-               .build();
-    }
+      UserDetails user1 = User
+              .withDefaultPasswordEncoder()
+              .username("kanchan")
+              .password("2078")
+              .roles("USER")
+              .build();
+
+      return new InMemoryUserDetailsManager(user1);
+   }
+   // or we can create class that implements UserDetailsService
+
 }
