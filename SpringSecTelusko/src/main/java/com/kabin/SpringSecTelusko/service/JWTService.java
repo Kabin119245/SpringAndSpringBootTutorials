71c8dc8f3336248ab4251e791f82525359f62677
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-09-11 19:38:40 +0545

    JWT token generated

--- SpringSecTelusko/src/main/java/com/kabin/SpringSecTelusko/service/JWTService.java
+++ SpringSecTelusko/src/main/java/com/kabin/SpringSecTelusko/service/JWTService.java
@@ -1,24 +1,54 @@
 package com.kabin.SpringSecTelusko.service;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
-
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class JWTService {
+
+    private String secretKey = "";
+
+    public JWTService() {
+        try {
+            KeyGenerator   keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey sk = keyGen.generateKey();
+            secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
     public String generateToken(String username) {
         //create claim
         Map<String, Object> claims = new HashMap<>();
 
-        return Jwts.builder()
-                .addClaims(claims)
-                .setSubject(username)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() * 60 * 60 * 30 ));
+            return Jwts.builder()
+                    .claims()
+                    .add(claims)
+                    .subject(username)
+                    .issuedAt(new Date(System.currentTimeMillis()))
+                    .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30 ))
+                    .and()
+                    .signWith(getKey())
+                    .compact();
 
+    }
+
+    private Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
 
     }
 }
